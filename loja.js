a43fd08bbc2fcaff855d4b145a06f1415873383e
moises lima <moiseskeko65@gmail.com>
2024-04-05 15:46:40 -0300

    quase la

--- loja.js
+++ loja.js
@@ -31,63 +31,32 @@ function displayCart() {
           cartList.appendChild(cartItem);
       });
 
-      // Exibir campo de cupom apenas se houver produtos no carrinho
       document.getElementById('cupomContainer').style.display = 'block';
   } else {
       cartList.innerHTML = '<p>Carrinho vazio</p>';
-      // Ocultar campo de cupom se o carrinho estiver vazio
       document.getElementById('cupomContainer').style.display = 'none';
   }
 }
 
-// Variável para controlar se o cupom já foi usado
 let cupomUsado = false;
 
-function aplicarCupom() {
-  if (!cupomUsado) {
-      const cupomInput = document.getElementById('cupomInput').value;
-      
-      // Verificar se o cupom é válido (coloque sua lógica de validação aqui)
-      if (cupomInput === 'MEUCUPOM') {
-          // Aplicar desconto (coloque sua lógica de desconto aqui)
-          // Exemplo: 10% de desconto
-          const desconto = calcularTotal() * 0.1;
-          const totalComDesconto = calcularTotal() - desconto;
 
-          // Exibir mensagem de cupom aplicado com sucesso
-          document.getElementById('cupomMessage').textContent = `Cupom aplicado com sucesso! Desconto de 10% aplicado. Total atual: R$ ${totalComDesconto.toFixed(2)}`;
-
-          // Atualizar variável para indicar que o cupom foi usado
-          cupomUsado = true;
-      } else {
-          // Exibir mensagem de cupom inválido
-          document.getElementById('cupomMessage').textContent = 'Cupom inválido. Tente novamente.';
-      }
-  } else {
-      // Exibir mensagem de cupom já utilizado
-      document.getElementById('cupomMessage').textContent = 'Você já utilizou um cupom de desconto.';
-  }
-}
 function addToCart(event) {
   const product = event.target.parentNode;
   const productName = product.querySelector('h2').innerText;
   const productImage = product.querySelector('img').src;
   const productPrice = product.querySelector('p:nth-child(3)').innerText;
 
-  // Cria um objeto para representar o produto
   const productData = {
     name: productName,
     image: productImage,
     price: productPrice
   };
 
-  // Obtém o carrinho atual do armazenamento local ou cria um novo array vazio
   let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-  // Adiciona o produto ao carrinho
   cart.push(productData);
 
-  // Salva o carrinho atualizado de volta no armazenamento local
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
